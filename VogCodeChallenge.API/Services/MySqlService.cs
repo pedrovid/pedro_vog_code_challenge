2fe3d2d2bbea48d945d2d11a1e29931f898a0ab4
Pedro Vidal <a317806@uach.mx>
2022-03-04 15:02:08 -0700

    [VOG-06] Service for a different DB use

---
+++ VogCodeChallenge.API/Services/MySqlService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using VogCodeChallenge.API.Context;
+
+namespace VogCodeChallenge.API.Services
+{
+
+    // Something like this will be the necessary service to change DB
+    public static class MySqlService
+    {
+        public static IServiceCollection AddMySql(this IServiceCollection services)
+        {
+            string connectionString = "";
+            services.AddDbContext<VogDbContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+            return services;
+        }
+    }
+}
