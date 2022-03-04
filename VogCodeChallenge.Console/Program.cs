3d8b471376e56e412e0588d9ebc533f5eefeafa4
Pedro Vidal <a317806@uach.mx>
2022-03-04 15:05:09 -0700

    [VOG-07] Console project to iterate list implemented

---
+++ VogCodeChallenge.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VogCodeChallenge.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuestionClass.RecursivelyIterateNames();
+            QuestionClass.WhileApproach();
+        }
+    }
+}
---
+++ VogCodeChallenge.Console/QuestionClass.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VogCodeChallenge.Console
+{
+    public static class QuestionClass
+    {
+        static List<string> NameList = new List<string>()
+        {
+            "Jimmy",
+            "Jeffrey",
+            "John",
+        };
+
+        /// <summary>
+        ///     Function to iterate NameList property recursively.
+        /// </summary>
+        public static void RecursivelyIterateNames()
+        {
+            System.Console.WriteLine("**** Recursive ****\n");
+
+            int itemsInList = NameList.Count;
+            if (itemsInList == 0) return;
+            if(itemsInList == 1)
+            {
+                System.Console.WriteLine(NameList.ElementAt(0));
+                return;
+            }
+
+            int middle = (itemsInList / 2);
+            RecursiveApproach(NameList.GetRange(0, middle));
+            RecursiveApproach(NameList.GetRange(middle, (itemsInList - middle)));
+            
+        }
+
+        /// <summary>
+        ///     Assistance function to iterate NameList property and show in console its values recursively.
+        /// </summary>
+        /// <param name="names">
+        ///     Segment of the NameList property.
+        /// </param>
+        private static void RecursiveApproach(List<string> names)
+        {
+            int itemsInList = names.Count;
+
+            if(itemsInList == 1)
+            {
+                System.Console.WriteLine(names.ElementAt(0));
+                return;
+            }
+
+            int middle = (itemsInList / 2) + 1;
+            if (itemsInList % 2 == 0)
+            {
+                RecursiveApproach(names.GetRange(0, itemsInList / 2));
+                RecursiveApproach(names.GetRange(middle - 1, (itemsInList - middle + 1)));
+            }
+            else
+            {
+                RecursiveApproach(names.GetRange(0, itemsInList / 2));
+                System.Console.WriteLine(names.ElementAt(itemsInList / 2));
+                RecursiveApproach(names.GetRange(middle, (itemsInList - middle)));
+            }
+        }
+
+        /// <summary>
+        ///     Function to iterate NameList property and show in console its values using a while loop.
+        /// </summary>
+        /// <param name="names">
+        ///     Segment of the NameList property.
+        /// </param>
+        public static void WhileApproach()
+        {
+            System.Console.WriteLine("\n**** While ****\n");
+            int i = 0;
+
+            while(NameList.ElementAtOrDefault(i) != null)
+            {
+                System.Console.WriteLine(NameList.ElementAt(i++));
+            };
+        }
+    }
+}
