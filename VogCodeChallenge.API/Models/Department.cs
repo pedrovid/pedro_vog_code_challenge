0b5c85de4e1fd1c1bcf2ac7fb74b4005d60c9240
Pedro Vidal <a317806@uach.mx>
2022-03-04 14:38:43 -0700

    [VOG-02] Models in project created

---
+++ VogCodeChallenge.API/Models/Department.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VogCodeChallenge.API.Models
+{
+    public class Department
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Index(IsUnique = true)]
+        public string Address { get; set; }
+
+        public List<Employee> Employees { get; set; }
+    }
+}
---
+++ VogCodeChallenge.API/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VogCodeChallenge.API.Models
+{
+    public class Employee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Required]
+        public string FirstName {get;set;}
+
+        [Required]
+        public string LastName {get;set;}
+
+        [Required]
+        public string JobTitle {get;set;}
+
+        [Required]
+        public string Address {get;set;}
+    }
+}
